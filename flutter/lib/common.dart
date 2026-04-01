045cb49f3d8c543fa726ca729d7088b8c3767186
B1ssmuth <ilybogdanow@yandex.ru>
2026-04-01 11:29:09 +0300

    Update accent colors and primary color scheme

--- flutter/lib/common.dart
+++ flutter/lib/common.dart
@@ -252,8 +252,8 @@ class MyTheme {
 
   static const Color grayBg = Color(0xFFEFEFF2);
   static const Color accent = Color(0xFF111111);
-  static const Color accent50 = Color(0x770071FF);
-  static const Color accent80 = Color(0xAA0071FF);
+  static const Color accent50 = Color(0x77111111);
+  static const Color accent80 = Color(0xAA111111);
   static const Color canvasColor = Color(0xFF212121);
   static const Color border = Color(0xFFCCCCCC);
   static const Color idColor = Color(0xFF111111);
@@ -454,7 +454,7 @@ class MyTheme {
         style:
             MenuStyle(backgroundColor: MaterialStatePropertyAll(Colors.white))),
     colorScheme: ColorScheme.light(
-        primary: Colors.blue, secondary: accent, background: grayBg),
+        primary: Color(0xFF111111), secondary: accent, background: grayBg),
     popupMenuTheme: PopupMenuThemeData(
         color: Colors.white,
         shape: RoundedRectangleBorder(
@@ -562,7 +562,7 @@ class MyTheme {
         style: MenuStyle(
             backgroundColor: MaterialStatePropertyAll(Color(0xFF121212)))),
     colorScheme: ColorScheme.dark(
-      primary: Colors.blue,
+      primary: Color(0xFF111111),
       secondary: accent,
       background: Color(0xFF24252B),
     ),
