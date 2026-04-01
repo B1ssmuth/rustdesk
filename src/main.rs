4104db90337dde544d833c05a3829c95114c115b
B1ssmuth <ilybogdanow@yandex.ru>
2026-04-01 10:37:55 +0300

    Rename application to 'yes-desk' and update metadata

--- src/main.rs
+++ src/main.rs
@@ -47,9 +47,9 @@ fn main() {
        -s, --server=[] 'Start server'",
     );
     let matches = App::new("rustdesk")
-        .version(crate::VERSION)
-        .author("Purslane Ltd<info@rustdesk.com>")
-        .about("RustDesk command line tool")
+        .name("yes-desk")
+        .author("YES Apart <i.bogdanov@yesapart.com>")
+        .about("YES Desk command line tool")
         .args_from_usage(&args)
         .get_matches();
     use hbb_common::{config::LocalConfig, env_logger::*};
